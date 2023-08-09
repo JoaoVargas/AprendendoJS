1a889ffb96547844372a01fef774f7faf97202ae
JoaoVargas <jotaguivargas@gmail.com>
2023-08-09 15:07:13 -0300

    inicializado terceiro projeto

---
+++ 3-Projetos/3.4-Stock App/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './assets/App.css'
+
+function App() {
+
+  return (
+    <h1>
+      Site
+    </h1>
+  )
+}
+
+export default App
